d2e2afb4f5ba7c73c9e4c39151f6f4824902d5ac
Santiago Henning <shenningmanrique@gmail.com>
2022-07-10 17:56:49 -0500

    bugfix

--- src/Frontend.js
+++ src/Frontend.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Select from 'react-select'
 
 class Login extends React.Component {
   constructor(props) {
@@ -138,6 +139,61 @@ class Signup extends React.Component {
   }
 }
 
+const states = [
+  { value: 'AK', label: 'Alaska'},
+  { value: 'TX', label: 'Texas'},
+  { value: 'AL', label: 'Alabama'},
+  { value: 'AR', label: 'Arkansas'},
+  { value: 'AZ', label: 'Arizona'},
+  { value: 'CA', label: 'California'},
+  { value: 'CO', label: 'Colorado'},
+  { value: 'CT', label: 'Connecticut'},
+  { value: 'DC', label: 'District of Columbia'},
+  { value: 'DE', label: 'Delaware'},
+  { value: 'FL', label: 'Florida'},
+  { value: 'GA', label: 'Georgia'},
+  { value: 'HI', label: 'Hawaii'},
+  { value: 'IA', label: 'Iowa'},
+  { value: 'ID', label: 'Idaho'},
+  { value: 'IL', label: 'Illinois'},
+  { value: 'IN', label: 'Indiana'},
+  { value: 'KS', label: 'Kansas'},
+  { value: 'KY', label: 'Kentucky'},
+  { value: 'LA', label: 'Louisiana'},
+  { value: 'MA', label: 'Massachusetts'},
+  { value: 'MD', label: 'Maryland'},
+  { value: 'ME', label: 'Maine'},
+  { value: 'MI', label: 'Michigan'},
+  { value: 'MN', label: 'Minnesota'},
+  { value: 'MO', label: 'Missouri'},
+  { value: 'MS', label: 'Mississippi'},
+  { value: 'MT', label: 'Montana'},
+  { value: 'NC', label: 'North Carolina'},
+  { value: 'ND', label: 'North Dakota'},
+  { value: 'NE', label: 'Nebraska'},
+  { value: 'NH', label: 'New Hampshire'},
+  { value: 'NJ', label: 'NewJersey'},
+  { value: 'NM', label: 'New Mexico'},
+  { value: 'NV', label: 'Nevada'},
+  { value: 'NY', label: 'New York'},
+  { value: 'OH', label: 'Ohio'},
+  { value: 'OK', label: 'Oklahoma'},
+  { value: 'OR', label: 'Oregon'},
+  { value: 'PA', label: 'Pennsylvania'},
+  { value: 'RI', label: 'Rhode Island'},
+  { value: 'SC', label: 'South Carolina'},
+  { value: 'SD', label: 'South Dakota'},
+  { value: 'TN', label: 'Tennessee'},
+  { value: 'TX', label: 'Texas'},
+  { value: 'UT', label: 'Utah'},
+  { value: 'VA', label: 'Virginia'},
+  { value: 'VT', label: 'Vermont'},
+  { value: 'WA', label: 'Washington'},
+  { value: 'WI', label: 'Wisconsin'},
+  { value: 'WV', label: 'West Virginia'},
+  { value: 'WY', label: 'Wyoming'}
+  ];
+
 class Profile extends React.Component {
   constructor(props) {
     super(props);
@@ -169,15 +225,16 @@ class Profile extends React.Component {
 
         <h1> Client Information </h1>
 
-        <form onSubmit={this.handleSubmit} className="d-flex flex-column">
+        <form name="clientInfo" onSubmit={this.handleSubmit} className="d-flex flex-column">
           <div className='form-group my-2 row'>
-            <label className="col-auto col-form-label">
+            <label className="col-4 col-form-label">
               Full Name
             </label>
-            <div className="col-auto">
+            <div className="col-8">
               <input 
                 type="text" 
-                name="fullName" 
+                name="fullName"
+                maxLength="50"
                 className='form-control' 
                 value={this.state.fullName}
                 onChange={this.handleInputChange}
@@ -186,13 +243,14 @@ class Profile extends React.Component {
           </div>
 
           <div className='form-group my-2 row'>
-            <label className="col-auto col-form-label">
+            <label className="col-4 col-form-label">
               Address 1
             </label>
-            <div className="col-auto">
+            <div className="col-8">
               <input 
                 type="text" 
                 name="address1" 
+                maxLength="100"
                 className='form-control' 
                 value={this.state.address1}
                 onChange={this.handleInputChange}
@@ -200,62 +258,68 @@ class Profile extends React.Component {
             </div>
           </div>
 
-          <div className='form-group my-2'>
-            <label>
-              Username
+          <div className='form-group my-2 row'>
+            <label className="col-4 col-form-label">
+              Address 2
+            </label>
+            <div className="col-8">
               <input 
                 type="text" 
-                name="username" 
+                name="address2" 
+                maxLength="100"
                 className='form-control' 
-                value={this.state.username} 
+                value={this.state.address2}
                 onChange={this.handleInputChange}
-                required />
-            </label>
+                />
+            </div>
           </div>
 
-          <div className='form-group my-2'>
-            <label>
-              Username
+          <div className='form-group my-2 row'>
+            <label className="col-4 col-form-label">
+              City
+            </label>
+            <div className="col-8">
               <input 
                 type="text" 
-                name="username" 
+                name="city" 
+                maxLength="100"
                 className='form-control' 
-                value={this.state.username} 
+                value={this.state.city}
                 onChange={this.handleInputChange}
                 required />
-            </label>
+            </div>
           </div>
 
-          <div className='form-group my-2'>
-            <label>
-              Username
+          <div className='form-group my-2 row'>
+            <label className="col-4 col-form-label">
+              State
+            </label>
+            <div className="col-8">
+              <Select options={states} />
+            </div>
+          </div>
+
+          <div className='form-group my-2 row'>
+            <label className="col-4 col-form-label">
+              Zip Code
+            </label>
+            <div className="col-8">
               <input 
                 type="text" 
-                name="username" 
+                name="zip" 
+                minLength="5"
+                maxLength="9"
                 className='form-control' 
-                value={this.state.username} 
+                value={this.state.zip}
                 onChange={this.handleInputChange}
                 required />
-            </label>
-          </div>
-
-          <div className='form-group my-2'>
-            <label>
-              Password
-              <input 
-                type="password" 
-                name="password" 
-                className='form-control' 
-                value={this.state.password} 
-                onChange={this.handleInputChange}
-                required />
-            </label>
+            </div>
           </div>
 
           <button 
             type="submit" 
             className="btn btn-success my-2">
-              Sign Up
+              Submit
           </button>
 
         </form>
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Profile />
+    <Signup />
 );
 
 // If you want to start measuring performance in your app, pass a function

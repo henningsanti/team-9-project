2b6cbc24580517a5ca63d92c0bcdae3930a597b8
Santiago Henning <shenningmanrique@gmail.com>
2022-07-23 19:37:00 -0500

    final merge adjustments

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {AuthProvider} from './context/AuthProvider';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
